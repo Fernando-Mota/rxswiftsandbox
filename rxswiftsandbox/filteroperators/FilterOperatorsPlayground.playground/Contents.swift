05ba6815637a45cd65eb1a6327c9d2a101de4669
Fernando Mota e Silva <fernando.mota@clickbus.com>
2017-12-29 14:50:01 -0200

    distinct operator

--- rxswiftsandbox/filteroperators/FilterOperatorsPlayground.playground/Contents.swift
+++ rxswiftsandbox/filteroperators/FilterOperatorsPlayground.playground/Contents.swift
@@ -140,8 +140,43 @@ example(of: "takeUntil") {
     subject.onNext("elemento 5")
 }
 
+example(of: "distinctUntilChanged") {
+    let disposeBag = DisposeBag()
+    
+    Observable.of("A", "A", "B", "B", "A")
+        .distinctUntilChanged()
+        .subscribe(onNext: { value in
+            print(value)
+        }).disposed(by: disposeBag)
+}
 
-
+example(of: "distinctUntilChanged(_:)") {
+    let disposeBag = DisposeBag()
+    
+    let formatter = NumberFormatter()
+    
+    formatter.numberStyle = .spellOut
+    
+    Observable<NSNumber>.of(10, 110, 20, 200, 210, 310)
+        .distinctUntilChanged{ a, b in
+            guard let aWords = formatter.string(from: a)?.components(separatedBy: " "), let bWords = formatter.string(from: b)?.components(separatedBy: " ") else {return false}
+            
+            var containsMatch = false
+            
+            for aWord in aWords {
+                for bWord in bWords {
+                    if aWord == bWord {
+                        containsMatch = true
+                        break
+                    }
+                }
+            }
+            return containsMatch
+        }
+        .subscribe(onNext: { value in
+            print(value)
+        }).disposed(by: disposeBag)
+}
 
 
 
